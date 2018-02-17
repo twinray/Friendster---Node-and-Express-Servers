1dbebefbcb5d49817c9e516c0ad81fced9c3b509
Jeff Ray <rayinsf@gmail.com>
2018-02-16 21:20:10 -0800

    fourth commit

---
+++ rockandrollfriendster/app/data/friends.js
@@ -0,0 +1,45 @@
+
+// i have no idea what the below is. 
+
+//{
+    //"name":"Ahmed",
+    //"photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    //"scores":[
+     //   5,
+      //  1,
+      //  4,
+       // 4,
+       // 5,
+       // 1,
+        //2,
+        //5,
+        //4,
+        //1
+      //]
+  //}
+  var friends = [ {
+    "name" : "Janis Joplin",
+    "photo" : "https://i.pinimg.com/736x/93/b2/b4/93b2b4e4b0b7e5125418fb1f25ba075b--janis-joplin-photo-sessions.jpg"
+}, {
+    "name" : "Joan Jet",
+    "photo" : "https://img.wennermedia.com/480-width/rs-193239-R1234_FEA_JETT_D.jpg",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "Tom Waits",
+    "photo" : "http://www.nndb.com/people/094/000024022/twaits-red.jpg",
+    "scores" : [5,5,5,5,5,5,5,5,5,5]
+}, {
+    "name" : "Patti Smith",
+    "photo" : "https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Patti_Smith_performing_in_Finland%2C_2007.jpg/320px-Patti_Smith_performing_in_Finland%2C_2007.jpg",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "Ozzy Osbourne",
+    "photo" : "https://www.billboard.com/files/media/Ozzy-Osbourne-London-1991-billboard-1548-650.jpg",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "Kieth Richards",
+    "photo" : "https://www.geeksandbeats.com/wp-content/uploads/2015/08/youngkeithrichards.jpg",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+
+}];
+
